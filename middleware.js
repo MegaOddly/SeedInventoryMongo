63618bd063f0c6d3668822f00dfb3d558957476f
Chase <zora1230@gmail.com>
2022-04-02 22:28:38 -0700

    Fix

--- middleware.js
+++ middleware.js
@@ -1,9 +1,6 @@
 
-
-
-
 module.exports.isLoggedIn = (req,res,next) => {
-    console.log("REQ.USER....", req.user);
+    
     if (!req.isAuthenticated()) {
         return res.redirect('/')
     }
@@ -12,7 +9,7 @@ module.exports.isLoggedIn = (req,res,next) => {
 
 
 module.exports.isEmployee = (req,res,next) => {
-    console.log("REQ.USER....", req.user);
+    
     if (req.user.role === 'Employee' || req.user.role === 'Administrator') {
         next();
     }else {
@@ -21,7 +18,7 @@ module.exports.isEmployee = (req,res,next) => {
     
 }
 module.exports.isAdmin = (req,res,next) => {
-    console.log("REQ.USER....", req.user);
+    
     if (req.user.role === 'Administrator') {
         next();
     }else {
